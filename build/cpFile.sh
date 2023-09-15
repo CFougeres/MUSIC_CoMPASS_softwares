3c731febb13f2ba763150a208df07b9a622b3df2
Chloé Fougères <chloe.fougeres@gmail.com>
2023-09-15 18:38:03 -0500

    Delete build directory

--- build/cpFile.sh
+++
@@ -1,6 +0,0 @@
-echo "Folder filer copy"
-for ((fileC=$2+1; fileC<=$3;fileC++))
-do
-	`cp -r ../RootFiles/Event/Run_$1/file$2/ ../RootFiles/Event/Run_$1/file$fileC`
-done
-echo "done"
--- build/mkRunDir.sh
+++
@@ -1,4 +0,0 @@
-echo "Make run directory"
-`mkdir ../RootFiles/Event/Run_$1/`
-`mkdir ../RootFiles/Event/Run_$1/file$2/`
-echo "done"
