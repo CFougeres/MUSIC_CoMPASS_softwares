5120eb66a03adc328c2de11998db1f9997ef4dd7
Chloé Fougères <chloe.fougeres@gmail.com>
2023-12-01 08:16:47 -0600

    Update OnlineVisu.C

--- OnlineVisu.C
+++ OnlineVisu.C
@@ -31,10 +31,10 @@ void OnlineVisu()
     if(DetectorType==1){
 		data_path = pathRun + Form("/RootFiles/Raw/Data_R_%i_%i.root",RunNumber,FileNumber);
 	}
-	if(DetectorType==0){
-        if(FileNumber==0){		data_path = pathRun+ Form("/DAQ/runSi_%i/RAW/DataR_runSi_%i_%i.root",RunNumber, RunNumber);}
+    if(DetectorType==0){
+        if(FileNumber==0){        data_path = pathRun+ Form("/DAQ/runSi_%i/RAW/DataR_runSi_%i.root",RunNumber, RunNumber);}
         if(FileNumber>0){        data_path = pathRun+ Form("/DAQ/runSi_%i/RAW/DataR_runSi_%i_%i.root",RunNumber, RunNumber,FileNumber);}
-	}
+    }
 	cout<<data_path<<endl;    Tree_data->Add(data_path.c_str());
     //"""""""""""""""""""""
     //Data tree structure
